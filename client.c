37d021097d0992d2fa96f3e68c481b3acf38ffb1
maxdegers <maxdegers@gmail.com>
2024-02-07 15:19:15 +0100

    rm mesage for obligatori part

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: mbrousse <mbrousse@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 13:26:56 by mbrousse          #+#    #+#             */
-/*   Updated: 2024/02/07 12:48:15 by mbrousse         ###   ########.fr       */
+/*   Updated: 2024/02/07 15:18:38 by mbrousse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static void	handler_client(int signal)
 	}
 	if (signal == SIGUSR2)
 	{
-		ft_printf("Message Received\n");
+		return ;
 	}
 }
 
